6f6a2ecddf93fb8d5118e5bca984a59c75806541
Medvedina <79661871+Medvedina@users.noreply.github.com>
2024-11-30 13:22:25 +0300

    feat(calc.py): изменён метод работы с ошибками

--- calc.py
+++ calc.py
@@ -1,48 +1,55 @@
 def main():
-    print('***Турбо-калькулятор 3000***\n')
+    from service.service_functions import calc, mode_check, rand, notify_user
+    import errors.errors as er
 
-    from service.service_functions import mode_check, calc, rand
+    notify_user('***Турбо-калькулятор 3000***\n')
 
     while True:
-        print('Выберите режим:')
-        print('0 - выйти')
-        print('1 - калькулятор')
-        print('2 - генератор случайных чисел')
+        notify_user('Выберите режим:')
+        notify_user('0 - выйти')
+        notify_user('1 - калькулятор')
+        notify_user('2 - генератор случайных чисел')
 
-        choise = input()
+        choice = input()
 
 
-        if mode_check(choise) is False and choise != '0':
-            print('Нет такого режима\n')
+        if mode_check(choice) is False and choice != '0':
+            notify_user('Нет такого режима\n')
             continue
 
-        elif mode_check(choise) == 0:
-            print('Завершение работы')
+        elif mode_check(choice) == 0:
+            notify_user('Завершение работы')
             break
 
-        elif mode_check(choise) == 1:
-            print('Калькулятор')
-            print('Введите выражение, (например: (13 + 2 * 3 / (32 + 5) + 2 ^ 5) '
+        elif mode_check(choice) == 1:
+            notify_user('Калькулятор')
+            notify_user('Введите выражение, (например: (13 + 2 * 3 / (32 + 5) + 2 ^ 5) '
                   'или exit для выхода)')
             query = input()
             while query != 'exit':
-                result = str(calc(query))
-                print('Ответ:', result)
+                try:
+                    result = str(calc(query))
+                    notify_user(f'Ответ: {result}')
+                except er.BracketError as e:
+                    notify_user('Ошибка ввода. Закройте все скобки')
+                    query = 'exit'
+                    continue
+                #except er.DivZero(ZeroDivisionError) as e:
+                    #pass
                 query = input()
-
-        elif mode_check(choise) == 2:
-            print('Генератор случайных чисел')
+        elif mode_check(choice) == 2:
+            notify_user('Генератор случайных чисел')
             while True:
-                print('\nВведите диапазон генерируемого числа через '
+                notify_user('\nВведите диапазон генерируемого числа через '
                       'пробел (1 100)    exit - выход')
                 rand_range = input()
                 if rand_range != 'exit':
-                    print('Введите значения, которых генератор должен избегать, (2 31 3), '
+                    notify_user('Введите значения, которых генератор должен избегать, (2 31 3), '
                           'Если таких значений нет, нажмите Enter     exit - выход')
                     ban_list = input()
                     if ban_list != 'exit':
                         result = rand(rand_range, ban_list)
-                        print('\n---- Число:', result)
+                        notify_user(f'\nЧисло: {result}')
                     else:
                         break
                 else:
