b59b85b0de29469ff0c0c4b95a9986a97d2547a3
Medvedina <79661871+Medvedina@users.noreply.github.com>
2024-12-13 11:33:32 +0300

    refactor(logs_script.py): удалена установка уровня за ненадобностью

--- history/logs_script.py
+++ history/logs_script.py
@@ -1,7 +1,6 @@
 import logging
 
 logger = logging.getLogger('calc')
-logger.setLevel(logging.DEBUG)
 
 handler_calc = logging.FileHandler('history/logs.txt', mode='a')
 formatter_calc = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
