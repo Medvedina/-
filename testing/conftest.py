a7b6604758ff6f922aa20be1fe01f29c48e81899
Medvedina <79661871+Medvedina@users.noreply.github.com>
2024-12-14 12:24:27 +0300

    tests(conftest.py): виджеты обновляются сразу после инициализации

--- testing/conftest.py
+++ testing/conftest.py
@@ -5,6 +5,7 @@ from beautiful_calc import create_app
 @pytest.fixture(scope='module', autouse=True)
 def app():
     window = create_app()
+    window.update_idletasks()
     yield window
     window.quit()
     window.destroy()
